9e3f44ad93300273e92e7dad496b36d21fe85a78
Yamada <ymd_h@outlook.jp>
2022-06-21 18:59:53 +0900

    Update static config

--- extodide/__init__.py
+++ extodide/__init__.py
@@ -5,11 +5,20 @@ from setuptools.command.build_ext import build_ext as _build_ext
 
 
 def cpython_get_include() -> str:
-    return f"{os.dirname(__file__)}/cpython"
+    return f"{os.path.dirname(__file__)}/cpython"
 
 def numpy_get_include() -> str:
-    return f"{os.dirname(__file__)}/numpy"
+    return f"{os.path.dirname(__file__)}/numpy"
 
+
+def LDFLAGS() -> List[str]:
+    return ["-s", "MODULARIZE=1",
+            "-s", "LINKABLE=1",
+            "-s", "EXPORT_ALL=1",
+            "-s", "WASM=1",
+            "-s", "LZ4=1",
+            "-s", "WASM_BIGINT",
+            "-s", "SIDE_MODULE=1"]
 
 class build_ext(_build_ext):
     pass
