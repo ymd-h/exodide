5e90aa0488142bba26889e2240d62ca5e437fbd3
Yamada <ymd_h@outlook.jp>
2022-06-21 19:00:10 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ from setuptools import setup, find_packages
 
 setup(name="extodide",
       description="build_ext for Pyodide",
-      version"0.0.0",
+      version="0.0.0",
       packages=find_packages(),
-      package_data={"extodide": ["cpython", "numpy"]})
+      package_data={"extodide":
+                    ["cpython/*.h",
+                     "cpython/cpython/*.h",
+                     "cpython/internal/*.h",
+                     "numpy/numpy/*.h",
+                     "numpy/numpy/libdivide/*",
+                     "numpy/numpy/random/*.h"]},
+      include_package_data=True)
