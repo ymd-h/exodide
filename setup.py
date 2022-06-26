5fe9086358246518ad008986e078a9b31493f33b
Yamada <ymd_h@outlook.jp>
2022-06-26 12:19:02 +0900

    Add wheel to dependency

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(name="exodide",
       description="build_ext for Pyodide",
       version="0.0.0",
+      install_requires=["wheel"],
       packages=find_packages(),
       package_data={"exodide":
                     ["cpython/*.h",
