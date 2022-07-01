131ea813664e24fe18321613a0f962c0071ab93b
Yamada <ymd_h@outlook.jp>
2022-07-01 19:29:23 +0900

    Add exodide package docstring

---
+++ exodide/__init__.py
@@ -0,0 +1,12 @@
+"""
+exodide: build_ext for Pyodide
+
+This package provides functionalities of building and using C/C++
+extension packages inside Pyodide.
+
+See Also
+--------
+build : Module for build wheel (.whl)
+install : Module for install wheel (.whl)
+inspect : Module for inspect shared object (.so)
+"""
