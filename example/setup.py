2272011fe85fdb8361bd8cc2fa2762f37d4f03a7
Yamada <ymd_h@outlook.jp>
2022-07-01 23:53:38 +0900

    Add include directory for pybind11

--- example/setup.py
+++ example/setup.py
@@ -1,6 +1,9 @@
+import os
 from setuptools import setup, find_packages, Extension
+
 from exodide import build
 
+include = os.path.join(os.path.dirname(__file__), "pybind11", "include")
 
 setup(name="exodide-example",
       version="0.0.0",
@@ -8,5 +11,6 @@ setup(name="exodide-example",
       cmdclass=build.cmdclass(),
       ext_modules=[Extension("exodide_example.example",
                              sources=["exodide-example/example.cc"],
-                             extra_compile_args=["-std=c++17"],
+                             extra_compile_args=["-std=c++17",
+                                                 f"-I{include}"],
                              language="c++")])
