eb89bbea7fc9a2dce5f3c1ad1e746bdc6805bb69
Yamada <ymd_h@outlook.jp>
2022-07-02 12:12:46 +0900

    Add long_description for PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import os
 from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+    README = f.read()
+
 
 setup(name="exodide",
       author="Yamada Hiroyuki",
       description="build_ext for Pyodide",
+      long_description=README,
+      long_description_content_type="text/markdown",
       version="0.0.0",
       url="https://github.com/ymd-h/exodide",
       install_requires=["wheel"],
