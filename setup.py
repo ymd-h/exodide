e1074509c5a0c1813de37d8c50d2a84c884e8fe5
Yamada <ymd_h@outlook.jp>
2022-07-02 11:09:59 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name="exodide",
+      author="Yamada Hiroyuki",
       description="build_ext for Pyodide",
       version="0.0.0",
+      url="https://github.com/ymd-h/exodide",
       install_requires=["wheel"],
       packages=find_packages(),
       package_data={"exodide":
@@ -14,4 +16,10 @@ setup(name="exodide",
                      "numpy/numpy/libdivide/*",
                      "numpy/numpy/random/*.h",
                      "numpy/LICENSE*"]},
+      classifiers=["Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "Development Status :: 4 - Beta",
+                   "Operating System :: OS Independent",
+                   "Intended Audience :: Developers",
+                   "Topic :: Software Development :: Libraries"],
       include_package_data=True)
