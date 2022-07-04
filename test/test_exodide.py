c57a7ce9cf9a7cd4affe5147e36b19fe0d99e889
Yamada <ymd_h@outlook.jp>
2022-07-04 22:46:54 +0900

    Add test script mock

---
+++ test/test_exodide.py
@@ -0,0 +1,18 @@
+import unittest
+
+import exodide
+
+class TestBuild(unittest.TestCase):
+    pass
+
+
+class TestInstall(unittest.TestCase):
+    pass
+
+
+class TestInspect(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
