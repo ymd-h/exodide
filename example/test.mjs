749dde8471df3ca4249cd31cd961653dfa0437db
Yamada <ymd_h@outlook.jp>
2022-07-06 00:04:23 +0900

    Remove try block for loadPackage

--- example/test.mjs
+++ example/test.mjs
@@ -1,14 +1,7 @@
 const { loadPyodide } = await import("pyodide/pyodide.js");
 const pyodide = await loadPyodide({ indexURL: "/pyodide-node/pyodide" });
 
-console.log("before loadPackage");
-try{
-    await pyodide.loadPackage("micropip");
-}catch(error){
-    console.error({error});
-    throw error;
-}
-console.log("after loadPackage");
+await pyodide.loadPackage("micropip");
 
 try{
     const ret = await pyodide.runPythonAsync(await (await fetch("http://localhost:8080/example/test_example.py")).text());
