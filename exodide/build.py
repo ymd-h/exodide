75cb1735d9d187d3694a7c19e624ee9b587cc477
Yamada <ymd_h@outlook.jp>
2022-07-09 22:09:32 +0900

    Refactoring: Convert system_include to function

--- exodide/build.py
+++ exodide/build.py
@@ -15,7 +15,8 @@ from distutils.command.build import build as _build
 from setuptools.command.build_ext import build_ext as _build_ext
 from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
-system_include = os.path.join(sys.prefix, "include", "python")
+def system_include():
+    return os.path.join(sys.prefix, "include", "python")
 
 def cpython_get_include() -> str:
     return f"{os.path.dirname(__file__)}/cpython"
@@ -58,7 +59,7 @@ class build_ext(_build_ext):
         self.include_dirs = [
             cpython_get_include(),
             numpy_get_include()
-        ] + [d for d in self.include_dirs if (system_include not in d)]
+        ] + [d for d in self.include_dirs if (system_include() not in d)]
 
         for ext in self.extensions:
             ext.extra_link_args = ext.extra_link_args + LDFLAGS()
