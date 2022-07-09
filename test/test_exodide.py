76e240464242062e8f1cc61e578334e876113d19
Yamada <ymd_h@outlook.jp>
2022-07-10 00:14:59 +0900

    Add exodide.build test

--- test/test_exodide.py
+++ test/test_exodide.py
@@ -1,12 +1,31 @@
 import unittest
+import os
 import platform
+import sys
 
 from exodide import build, inspect
 
 class TestBuild(unittest.TestCase):
-    def cmdclass(self):
+    def test_cmdclass(self):
         self.assertEqual(build.cmdclass(),
                          {"build": build.build, "build_ext": build.build_ext})
+
+    def test_system_include(self):
+        self.assertIsInstance(build.system_include(), str)
+        self.assertEqual(build.system_include(), build.system_include())
+        self.assertEqual(build.system_include(),
+                         os.path.join(sys.path, "include", "python"))
+
+    def test_exodide_include(self):
+        self.assertIsInstance(build.exodide_include(), list)
+        self.assertEqual(len(build.exodide_include()), 2)
+        self.assertEqual(list(map(lambda s: os.path.basename(s),
+                                  build.exodide_include())),
+                         ["cpython", "numpy"])
+
+    def test_adjust_include(self):
+        self.assertEqual(build.adjust_include(build.system_include()),
+                         build.exodide_include())
 
     def test_plat_name(self):
         self.assertEqual(build.plat_name(), "emscripten-wasm32")
